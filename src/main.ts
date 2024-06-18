59ec766dd1234ec47f1951cd6667cf2ebc050ae4
yangpq <2825925353@qq.com>
2024-06-18 23:49:07 +0800

    fix:配置文件更改

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from '@/App.vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 // @ts-expect-error忽略当前文件ts类型的检测否则有红色提示(打包会失败)
